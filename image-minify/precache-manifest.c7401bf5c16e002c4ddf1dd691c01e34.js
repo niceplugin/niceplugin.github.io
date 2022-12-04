3125abb9d5897d7cf3df86e51194cd31687416ea
Niceplugin <niceplugin@gmail.com>
2022-12-04 19:18:00 +0900

    add site: image-minify

---
+++ image-minify/precache-manifest.c7401bf5c16e002c4ddf1dd691c01e34.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "dd75228fef4ba29cfc16",
+    "url": "/image-minify/css/app.58789c06.css"
+  },
+  {
+    "revision": "09958c8f7912c1c463e3",
+    "url": "/image-minify/css/chunk-vendors.381007ee.css"
+  },
+  {
+    "revision": "9cd13b8619e42027bad6957547b5f092",
+    "url": "/image-minify/img/logo.9cd13b86.svg"
+  },
+  {
+    "revision": "1c81fab553a2afb07202db3a61fbb504",
+    "url": "/image-minify/img/og_image.png"
+  },
+  {
+    "revision": "0aeb33159e4eae8188ec21900a3746d1",
+    "url": "/image-minify/index.html"
+  },
+  {
+    "revision": "dd75228fef4ba29cfc16",
+    "url": "/image-minify/js/app.b071b2d2.js"
+  },
+  {
+    "revision": "09958c8f7912c1c463e3",
+    "url": "/image-minify/js/chunk-vendors.f5fb3f74.js"
+  },
+  {
+    "revision": "7e488b80694f0efc230c0d5903e51ed3",
+    "url": "/image-minify/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/image-minify/robots.txt"
+  }
+]);
